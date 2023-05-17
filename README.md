ef78276c0ddc8ada075d098ce1e46a066ab2a813
Sainozhenko <dmytrosainozhenko@gmail.com>
2023-05-17 17:02:30 +0200

    conultation17-05

--- README.md
+++ README.md
@@ -18,3 +18,4 @@
 |  11/05/2023   | -[Work10](https://github.com/Sainozhenko/frontend/tree/master/10_JS_Arrays_Func_11_05_2023)| - |
 |  12/05/2023   | -[Work11](https://github.com/Sainozhenko/frontend/tree/master/11_JS_Objects_12_05_2023)    | -[Consultation8](https://github.com/Sainozhenko/frontend/tree/master/seminars/Objects_12_05_2023) |
 |  15/05/2023   | -[Work12](https://github.com/Sainozhenko/frontend/tree/master/12_JS_Event_Counter_15_05_2023)    | -[Work12.1](https://github.com/Sainozhenko/frontend/tree/master/12_JS_TODO_15_05_2023) |
+|  17/05/2023   | -[Work13](https://github.com/Sainozhenko/frontend/tree/master/13_JS_Promies_17_05_2023)    | -[Work13.1](https://github.com/Sainozhenko/frontend/tree/master/13_JS_JSONPlaceHolder_17_05_2023) |
--- seminars/JSONPlaceHolder_Albums_17_05_2023/script.js
+++ seminars/JSONPlaceHolder_Albums_17_05_2023/script.js
@@ -1,3 +1,32 @@
 const url = 'https://jsonplaceholder.typicode.com/';
 
-fetch(`${url}Albums`){}
+// fetch(`${url}albums`)
+//     .then(res => res.json())
+//     .then(albums => {
+//         const albumList = document.getElementById('albumList');
+
+//         albums.forEach(album => {
+//             const albumCard = document.createElement('div');
+//             albumCard.classList.add('albumCard');
+
+//             const title = document.createElement('h3');
+//             title.classList.add('title');
+//             title.textContent = album.title;
+
+//             const photoList = document.createElement('div');
+//             photoList.classList.add('photoList');
+//             // https://jsonplaceholder.typicode.com/albums/1/photos
+//             fetch(`${url}/albums/${album.id}/photos`)
+//                 .then(res => res.json())
+//                 .then(photos => {
+//                     photos.forEach(photo => {
+//                         const photoItem = document.createElement('div');
+//                         photoItem.classList.add('photoItem');
+
+//                         const a = document.createElement('a');
+//                         a.classList.add('a');
+                        
+//                     })
+//                 })
+//         })
+//     })
